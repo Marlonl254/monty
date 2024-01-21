d21d18cabd516dbc99ed7f8dd5485526fc5c45c1
Joepackins Owino <88088880+Joedgan12@users.noreply.github.com>
2024-01-21 23:16:42 +0300

    Update main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ bus_t bus = {NULL, NULL, NULL, 0};
 * main - monty code interpreter
 * @argc: number of arguments
 * @argv: monty file location
-* Return: 0 on success
+* Return: success (0)
 */
 int main(int argc, char *argv[])
 {
