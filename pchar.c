876c5de5ee8f98c85697f4bf7018dffa309b37d0
Joepackins Owino <88088880+Joedgan12@users.noreply.github.com>
2024-01-21 23:06:19 +0300

    Update pchar.c

--- pchar.c
+++ pchar.c
@@ -1,10 +1,10 @@
 #include "monty.h"
 /**
- * f_pchar - prints the char at the top of the stack,
+ * f_pchar - print char at the top of the stack,
  * followed by a new line
  * @head: stack head
  * @counter: line_number
- * Return: no return
+ * Return: void
 */
 void f_pchar(stack_t **head, unsigned int counter)
 {
