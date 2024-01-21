c561188d79adb7836d8f07cbcaaa0e92eba96777
Joepackins Owino <88088880+Joedgan12@users.noreply.github.com>
2024-01-21 23:15:54 +0300

    Update mod.c

--- mod.c
+++ mod.c
@@ -1,10 +1,10 @@
 #include "monty.h"
 /**
- * f_mod - computes the rest of the division of the second
+ * f_mod - compute rest of the division of the second
  * top element of the stack by the top element of the stack
  * @head: stack head
  * @counter: line_number
- * Return: no return
+ * Return: void
 */
 void f_mod(stack_t **head, unsigned int counter)
 {
