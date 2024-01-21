4451b7137e2bcda7f9fd50b1b6e9a47d83453480
Joepackins Owino <88088880+Joedgan12@users.noreply.github.com>
2024-01-21 23:03:20 +0300

    Update queue.c

--- queue.c
+++ queue.c
@@ -13,9 +13,9 @@ void f_queue(stack_t **head, unsigned int counter)
 }
 
 /**
- * addqueue - add node to the tail stack
+ * addqueue - add node to tail stack
  * @n: new_value
- * @head: head of the stack
+ * @head: head of stack
  * Return: no return
 */
 void addqueue(stack_t **head, int n)
