6543e3f3b5dc3d68b0e0db6440f427b83ce719ad
Joepackins Owino <88088880+Joedgan12@users.noreply.github.com>
2024-01-21 23:07:20 +0300

    Update pall.c

--- pall.c
+++ pall.c
@@ -1,9 +1,9 @@
 #include "monty.h"
 /**
- * f_pall - prints the stack
+ * f_pall - print stack
  * @head: stack head
  * @counter: no used
- * Return: no return
+ * Return: void
 */
 void f_pall(stack_t **head, unsigned int counter)
 {
